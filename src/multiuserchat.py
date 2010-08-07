a7f545889932d82fbb26de1a043e4c752f6fc3ca
louiz@4325f9fc-e183-4c21-96ce-0ab188b42d13 <louiz@4325f9fc-e183-4c21-96ce-0ab188b42d13>
2010-08-07 23:30:58 +0000

    update version

--- src/multiuserchat.py
+++ src/multiuserchat.py
@@ -316,7 +316,7 @@ class MultiUserChat(object):
         qp = iq_obj.getTag('query')
         if config.get('send_poezio_info', 'true') == 'true':
             qp.setTagData('name', 'Poezio')
-            qp.setTagData('version', '0.6.2')
+            qp.setTagData('version', '0.7 dev')
         else:
             qp.setTagData('name', 'Unknown')
             qp.setTagData('version', 'Unknown')
