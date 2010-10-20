dc61d36cf109828ca3a60dfe4b65e44b3162b9b8
louiz@4325f9fc-e183-4c21-96ce-0ab188b42d13 <louiz@4325f9fc-e183-4c21-96ce-0ab188b42d13>
2010-10-20 17:33:13 +0000

    fix an import issue (JID)

--- src/contact.py
+++ src/contact.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Poezio.  If not, see <http://www.gnu.org/licenses/>.
 
-from sleekxmpp.xmlstream.jid import JID
+from sleekxmpp.xmlstream.stanzabase import JID
 
 class Resource(object):
     """
