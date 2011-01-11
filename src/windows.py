26b38c810771f84b4deb7cd96a67733d71722222
Florent Le Coz <louiz@louiz.org>
2011-01-11 13:42:14 +0100

    Limit nick size to the user list width

--- src/windows.py
+++ src/windows.py
@@ -136,7 +136,7 @@ class UserList(Win):
                 else:
                     show_col = self.color_show[user.show]
                 self.addstr(y, 0, theme.CHAR_STATUS, curses.color_pair(show_col))
-                self.addstr(y, 1, user.nick, curses.color_pair(role_col))
+                self.addstr(y, 1, user.nick[:self.width-1], curses.color_pair(role_col))
                 y += 1
                 if y == self.height:
                     break
