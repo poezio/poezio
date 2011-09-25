7ee5f251d927a5e0d5da352810e7686b56eb2b57
Florent Le Coz <louiz@louiz.org>
2011-09-25 19:30:09 +0200

    Restaure that

--- src/tabs.py
+++ src/tabs.py
@@ -1002,6 +1002,7 @@ class PrivateTab(ChatTab):
         #self.commands['info'] = (self.command_info, _('Usage: /info\nInfo: Display some information about the user in the MUC: '), None)
         self.commands['unquery'] = (self.command_unquery, _("Usage: /unquery\nUnquery: close the tab"), None)
         self.commands['part'] = (self.command_unquery, _("Usage: /part\nPart: close the tab"), None)
+        self.commands['version'] = (self.command_version, _('Usage: /version\nVersion: get the software version of the current interlocutor (usually its XMPP client and Operating System)'), None)
         self.resize()
         self.on = True
 
@@ -1032,6 +1033,21 @@ class PrivateTab(ChatTab):
         /unquery
         """
         self.core.close_tab()
+
+    def command_version(self, arg):
+        """
+        /version
+        """
+        def callback(res):
+            if not res:
+                return self.core.information('Could not get the software version from %s' % (jid,), 'Warning')
+            version = '%s is running %s version %s on %s' % (jid,
+                                                             res.get('name') or _('an unknown software'),
+                                                             res.get('version') or _('unknown'),
+                                                             res.get('os') or _('on an unknown platform'))
+            self.core.information(version, 'Info')
+        jid = self.get_room().name
+        self.core.xmpp.plugin['xep_0092'].get_version(jid, callback=callback)
 
     def resize(self):
         if self.core.information_win_size >= self.height-3 or not self.visible:
