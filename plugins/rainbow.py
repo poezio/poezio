305e5ed40d129843a774f14990cf6f46d8c6e1a8
Florent Le Coz <louiz@louiz.org>
2011-11-07 19:59:12 +0100

    Add a funny rainbow plugin, mainly to test some plugin hooks.

---
+++ plugins/rainbow.py
@@ -0,0 +1,19 @@
+from plugin import BasePlugin
+import random
+
+possible_colors = list(range(256))
+# remove the colors that are almost white or almost black
+for col in [16, 232, 233, 234, 235, 236, 237, 15, 231, 255, 254, 253, 252, 251]:
+    possible_colors.remove(col)
+
+def rand_color():
+    return '\x19%s}' % (random.choice(possible_colors),)
+
+class Plugin(BasePlugin):
+    def init(self):
+        self.add_poezio_event_handler('muc_say', self.rainbowize)
+        self.add_poezio_event_handler('private_say', self.rainbowize)
+        self.add_poezio_event_handler('conversation_say', self.rainbowize)
+
+    def rainbowize(self, msg):
+        msg['body'] = ''.join(['%s%s' % (rand_color(),char,) for char in msg['body']])
