572cc678a079aa24fffebda5c76f4e5cb35893c5
mathieui <mathieui@mathieui.net>
2011-11-13 00:26:14 +0100

    Example for add_tab_command in the test plugin

--- plugins/test.py
+++ plugins/test.py
@@ -1,8 +1,10 @@
 from plugin import BasePlugin
+import tabs
 
 class Plugin(BasePlugin):
     def init(self):
         self.add_command('plugintest', self.command_plugintest, 'Test command')
+        self.add_tab_command(tabs.MucTab, 'plugintest', self.command_tab_plugintest, 'Test command')
         self.add_event_handler('message', self.on_message)
         self.core.information("Plugin loaded")
 
@@ -11,6 +13,10 @@ class Plugin(BasePlugin):
 
     def on_message(self, message):
         self.core.information("Test plugin received message: {}".format(message))
+
+    def command_tab_plugintest(self, args):
+        self.core.information("Command for MucTabs! With args {}".format(args))
+        self.del_tab_command(tabs.MucTab, 'plugintest')
 
     def command_plugintest(self, args):
         self.core.information("Command! With args {}".format(args))
