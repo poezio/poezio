b7cbe7d392829a953a73d700ae58c556d68ac74a
mathieui <mathieui@mathieui.net>
2012-02-24 02:22:02 +0100

    Fixes #2334

--- src/tabs.py
+++ src/tabs.py
@@ -701,6 +701,7 @@ class MucTab(ChatTab):
     def command_configure(self, arg):
         form = self.core.xmpp.plugin['xep_0045'].getRoomForm(self.get_name())
         if not form:
+            self.core.information('Could not retrieve the configuration form', 'Error')
             return
         self.core.open_new_form(form, self.cancel_config, self.send_config)
 
