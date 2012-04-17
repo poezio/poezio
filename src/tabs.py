1fdb91a98393aa0ad2f9b844cec7d714b4a3f2f7
mathieui <mathieui@mathieui.net>
2012-04-18 00:18:18 +0200

    Split correctly on command_groupremove

--- src/tabs.py
+++ src/tabs.py
@@ -1855,7 +1855,7 @@ class RosterInfoTab(Tab):
         """
         Remove the specified JID to the specified group
         """
-        args = args.split(None, 1)
+        args = common.shell_split(args)
         if len(args) != 2:
             return
         jid = JID(args[0]).bare
