31382401500292c27797d21d8e86bc7c2fe48161
mathieui <mathieui@mathieui.net>
2012-04-18 00:16:44 +0200

    Do not split on command_unignore as it is unnecessary

--- src/tabs.py
+++ src/tabs.py
@@ -1001,11 +1001,10 @@ class MucTab(ChatTab):
         """
         /unignore <nick>
         """
-        args = common.shell_split(arg)
-        if len(args) != 1:
+        if not arg:
             self.core.command_help('unignore')
             return
-        nick = args[0]
+        nick = arg
         user = self.get_user_by_name(nick)
         if not user:
             self.core.information(_('%s is not in the room') % nick)
