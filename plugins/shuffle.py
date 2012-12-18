ca7489fd8f17dbd437709bc81e3bad8ffef94345
Florent Le Coz <louiz@louiz.org>
2012-12-18 02:03:24 +0100

    super useful plugin that shuffles the message you send.

---
+++ plugins/shuffle.py
@@ -0,0 +1,11 @@
+from plugin import BasePlugin
+from random import shuffle
+
+class Plugin(BasePlugin):
+    def init(self):
+        self.add_event_handler('muc_say', self.shuffle)
+
+    def shuffle(self, msg, tab):
+        split = msg['body'].split()
+        shuffle(split)
+        msg['body'] = ' '.join(split)
