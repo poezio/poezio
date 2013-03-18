86d4874718ed84ae14aba9089850306768cead2a
Florent Le Coz <louiz@louiz.org>
2013-03-18 23:08:42 +0100

    Revstr works in all conversation tabs now.

--- plugins/revstr.py
+++ plugins/revstr.py
@@ -3,6 +3,8 @@ from plugin import BasePlugin
 class Plugin(BasePlugin):
     def init(self):
         self.api.add_event_handler('muc_say', self.revstr)
+        self.api.add_event_handler('conversation_say', self.revstr)
+        self.api.add_event_handler('private_say', self.revstr)
 
     def revstr(self, msg, tab):
         msg['body'] = msg['body'][::-1]
