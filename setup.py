bb00727e786dacaf9a5010d0256dc42051cfa636
mathieui <mathieui@mathieui.net>
2014-03-21 00:54:46 +0100

    Update setup.py (from what was changed for the pypi release)
    also remove old stuff from the default config

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from distutils.core import setup, Extension
 import os, sys
 
@@ -17,13 +18,16 @@ setup(name="poezio",
        version="0.8.3-dev",
        description="A console XMPP client",
        long_description=
-       """
-       Poezio is a Free chat client aiming to reproduce the ease of use of most
-       IRC clients (e.g. weechat, irssi) while using the XMPP network.
-       """,
+       "Poezio is a Free chat client aiming to reproduce the ease of use of most "
+       "IRC clients (e.g. weechat, irssi) while using the XMPP network."
+       "\n"
+       "Documentation is available at http://doc.poez.io/0.8.",
+
+
        ext_modules = [module_poopt],
-       url = 'http://poezio.eu/',
+       url = 'http://poez.io/',
        license = 'zlib',
+       download_url = 'https://dev.louiz.org/projects/poezio/files',
 
        author = 'Florent Le Coz',
        author_email = 'louiz@louiz.org',
@@ -32,13 +36,13 @@ setup(name="poezio",
        maintainer_email = 'mathieui@mathieui.net',
 
        classifiers = ['Development Status :: 2 - Pre-Alpha',
+                       'Topic :: Communications :: Chat',
                        'Environment :: Console :: Curses',
                        'Intended Audience :: End Users/Desktop',
                        'License :: OSI Approved :: zlib/libpng License',
                        'Natural Language :: English',
                        'Operating System :: Unix',
-                       'Topic :: Communications :: Chat',
-                       'Programming Language :: Python :: 3',
+                       'Programming Language :: Python :: 3'
                     ],
        keywords = ['jabber', 'xmpp', 'client', 'chat', 'im', 'console'],
        packages = ['poezio', 'poezio_plugins', 'poezio_plugins.gpg', 'poezio_themes'],
@@ -46,6 +50,11 @@ setup(name="poezio",
        package_data = {'poezio': ['default_config.cfg']},
        scripts = ['scripts/poezio', 'scripts/poezio_gpg_export'],
        data_files = [('share/man/man1/', ['data/poezio.1'])],
+
+       install_requires = ['sleekxmpp==1.2.4',
+                           'dnspython3>=1.11.1'],
+       extras_require = {'OTR plugin': 'python-potr>=1.0',
+                         'Screen autoaway plugin': 'pyinotify==0.9.4'}
 )
 
 # Remove the link afterwards
