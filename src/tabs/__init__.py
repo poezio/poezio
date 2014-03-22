918e15d1781fce0fbf2b7b2b047be32163bc4679
mathieui <mathieui@mathieui.net>
2014-03-22 17:20:15 +0100

    Fix a hidden traceback (NS_MUC_USER not exported in tabs/)

--- src/tabs/__init__.py
+++ src/tabs/__init__.py
@@ -1,6 +1,6 @@
 from . basetabs import Tab, ChatTab, GapTab, STATE_PRIORITY
 from . rostertab import RosterInfoTab
-from . muctab import MucTab
+from . muctab import MucTab, NS_MUC_USER
 from . privatetab import PrivateTab
 from . conversationtab import ConversationTab, StaticConversationTab,\
         DynamicConversationTab
