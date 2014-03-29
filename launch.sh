42769dd28feaf82ff3b803395d3f563188ab1484
mathieui <mathieui@mathieui.net>
2014-03-29 16:36:14 +0100

    Make launch.sh runnable from any directory

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -d "$PWD/.git" ]
+if [ -d "$(dirname $0)/.git" ]
 then
-    args=$(git show --format='%h %ci' | head -n1)
+    args=$(git --git-dir="$(dirname $0)/.git" show --format='%h %ci' | head -n1)
 else
     args="0.8.3-dev"
 fi
-exec python3 -OO src/poezio.py -v "$args" "$@"
+exec python3 -OO "$(dirname $0)/src/poezio.py" -v "$args" "$@"
 
