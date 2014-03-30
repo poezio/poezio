cf37785edc2de2c5d5c02919f48ada38c566ff45
mathieui <mathieui@mathieui.net>
2014-03-30 13:56:12 +0200

    Fix the launch script from directory with spaces in them

--- launch.sh
+++ launch.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+poezio_dir=$(dirname "$0")
 
-if [ -d "$(dirname $0)/.git" ]
+if [ -d "$poezio_dir/.git" ]
 then
-    args=$(git --git-dir="$(dirname $0)/.git" show --format='%h %ci' | head -n1)
+    args=$(git --git-dir="$poezio_dir/.git" show --format='%h %ci' | head -n1)
 else
     args="0.8.3-dev"
 fi
-exec python3 -OO "$(dirname $0)/src/poezio.py" -v "$args" "$@"
+exec python3 -OO "$poezio_dir/src/poezio.py" -v "$args" "$@"
 
