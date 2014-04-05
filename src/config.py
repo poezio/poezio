poezio/poezio

commit ca0950db07ff5006ba760e829f2fa6a7c2f8ea41

Remove unused imports
(thanks eijebong)

src/config.py
@@ -12,12 +12,9 @@ from/to the config file
 
 DEFSECTION = "Poezio"
 
-from gettext import gettext as _
-import sys
-import tempfile
-
-import os
 import logging
+import os
+import sys
 
 from configparser import RawConfigParser, NoOptionError, NoSectionError
 from os import environ, makedirs, path, remove

plugins/alias.py
@@ -68,9 +68,8 @@ Example of the syntax:
 
 
 from plugin import BasePlugin
-import common
 from common import shell_split
-from functools import partial, wraps
+
 
 class Plugin(BasePlugin):
     def init(self):

plugins/exec.py
@@ -36,9 +36,7 @@ Usage
 """
 
 from plugin import BasePlugin
-import os
 import common
-import shlex
 import subprocess
 
 class Plugin(BasePlugin):

plugins/ping.py
@@ -25,7 +25,6 @@ Command
 from plugin import BasePlugin
 from roster import roster
 from common import safeJID
-import common
 import tabs
 import time
 

plugins/random_nick.py
@@ -12,7 +12,6 @@ automatically be changed to something random, for example: ::
 """
 
 from plugin import BasePlugin
-from tabs import MucTab
 from random import choice
 
 class Plugin(BasePlugin):

plugins/replace_word.py
@@ -17,7 +17,6 @@ Just use the word in a message. It will be replaced automatically.
 """
 
 from plugin import BasePlugin
-import tabs
 import re
 
 class Plugin(BasePlugin):

plugins/uptime.py
@@ -12,7 +12,6 @@ Command
         Retrieve the uptime of the server of ``jid``.
 """
 from plugin import BasePlugin
-import tabs
 from common import parse_secs_to_str, safeJID
 from sleekxmpp.xmlstream import ET
 

setup.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     from distutils.core import setup, Extension
 
-import os, sys
+import os
 
 module_poopt = Extension('poezio.poopt',
                     extra_compile_args=['-Wno-declaration-after-statement'],

commit 587450939b5f2ab3b4cb46f44c6ab08e15722caf

Fix a bug introduced in ca0950d

src/config.py
@@ -15,6 +15,7 @@ DEFSECTION = "Poezio"
 import logging
 import os
 import sys
+from gettext import gettext as _
 
 from configparser import RawConfigParser, NoOptionError, NoSectionError
 from os import environ, makedirs, path, remove