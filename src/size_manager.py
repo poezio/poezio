9e72f8336da77c566fbbe6aad7caf6e79a0aab58
mathieui <mathieui@mathieui.net>
2014-04-29 00:29:54 +0200

    Add the size manager (forgotten in the previous commits)

---
+++ src/size_manager.py
@@ -0,0 +1,52 @@
+"""
+Size Manager:
+    used to check size boundaries of the whole window and
+    specific tabs
+"""
+from windows import g_lock
+
+THRESHOLD_WIDTH_DEGRADE = 45
+THRESHOLD_HEIGHT_DEGRADE = 10
+
+FULL_WIDTH_DEGRADE = 66
+FULL_HEIGHT_DEGRADE = 10
+
+class SizeManager(object):
+
+    def __init__(self, core, win_cls):
+        self._win_class = win_cls
+        self._core = core
+
+    @property
+    def tab_scr(self):
+        return self._win_class._tab_win
+
+    @property
+    def core_scr(self):
+        return self._core.stdscr
+
+    @property
+    def tab_degrade_x(self):
+        with g_lock:
+            _, x = self.tab_scr.getmaxyx()
+        return x < THRESHOLD_WIDTH_DEGRADE
+
+    @property
+    def tab_degrade_y(self):
+        with g_lock:
+            y, x = self.tab_scr.getmaxyx()
+        return y < THRESHOLD_HEIGHT_DEGRADE
+
+    @property
+    def core_degrade_x(self):
+        with g_lock:
+            y, x = self.core_scr.getmaxyx()
+        return x < FULL_WIDTH_DEGRADE
+
+    @property
+    def core_degrade_y(self):
+        with g_lock:
+            y, x = self.core_scr.getmaxyx()
+        return y < FULL_HEIGHT_DEGRADE
+
+
