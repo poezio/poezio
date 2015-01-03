d809ba05bf6fe597bcc6d526f82fc59ef6da63e3
mathieui <mathieui@mathieui.net>
2015-01-03 21:06:56 +0100

    Do not trigger the “tab_change” event when going over a gaptab

--- src/core/core.py
+++ src/core/core.py
@@ -1175,7 +1175,7 @@ class Core(object):
             self._current_tab_nb = len(self.tabs) - 1
         else:
             self._current_tab_nb = value
-        if old != self._current_tab_nb:
+        if old != self._current_tab_nb and self.tabs[self._current_tab_nb]:
             self.events.trigger('tab_change', old, self._current_tab_nb)
 
     ### Opening actions ###
