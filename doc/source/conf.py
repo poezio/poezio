c8b4d20a143862f6f57766546565c9b9b414696c
mathieui <mathieui@mathieui.net>
2015-08-01 20:31:46 +0200

    Update stuff to say 1.0-dev

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,9 +53,9 @@ copyright = '%s, Mathieu Pasquet - Florent Le Coz' % time.strftime('%Y')
 # built documents.
 #
 # The short X.Y version.
-version = '0.9'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.9'
+release = '1.0-dev'
 
 add_function_parentheses = True
 
