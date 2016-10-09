eefc126a0b0bc25af283c9e4aef56225d3b87a58
mathieui <mathieui@mathieui.net>
2016-10-09 18:19:19 +0200

    poezio 0.10 release

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ with open('README.rst', encoding='utf-8') as readme_fd:
     LONG_DESCRIPTION = readme_fd.read()
 
 setup(name="poezio",
-      version="0.10" + version,
+      version="0.10",
       description="A console XMPP client",
       long_description=LONG_DESCRIPTION,
       ext_modules=[module_poopt],
