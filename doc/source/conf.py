66f3cf630cdcef1af377c3d27dd4a7efe6ebcd79
mathieui <mathieui@mathieui.net>
2017-01-31 00:22:19 +0100

    Prepare for next version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,9 +53,9 @@ copyright = '%s, Mathieu Pasquet - Florent Le Coz' % time.strftime('%Y')
 # built documents.
 #
 # The short X.Y version.
-version = '0.11'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.11'
+release = '1.0-dev'
 
 add_function_parentheses = True
 
