c8ee85cca7d5fbb5ba40d6eff60031e1e8deaa2c
Georg Lukas <georg@op-co.de>
2017-03-28 10:47:04 +0200

    self-ping: consider 'feature-not-implemented' a valid ping response

--- poezio/tabs/muctab.py
+++ poezio/tabs/muctab.py
@@ -1697,7 +1697,7 @@ class MucTab(ChatTab):
                                                     timeout=60)
 
     def on_self_ping_result(self, iq):
-        if iq["type"] == "error":
+        if iq["type"] == "error" and iq["error"]["condition"] != "feature-not-implemented":
             self.command_cycle(iq["error"]["text"] or "not in this room")
             self.core.refresh_window()
         else:                   # Re-send a self-ping in a few seconds
