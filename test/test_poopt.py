ee5e807fc2be9175ae862302a3960d6fcb4337d9
mathieui <mathieui@mathieui.net>
2017-10-11 23:06:57 +0200

    Add a poopt.cut_text multiline unit test

--- test/test_poopt.py
+++ test/test_poopt.py
@@ -10,3 +10,7 @@ def test_cut_text():
 
     text = '12345678901234567890'
     assert cut_text(text, 5) == [(0, 5), (5, 10), (10, 15), (15, 20)]
+
+def test_cut_text():
+    text = 'a\nb\nc\nd'
+    assert cut_text(text, 10) == [(0, 2), (2, 4), (4, 6), (6, 7)]
