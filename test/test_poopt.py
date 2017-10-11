0a4d66a65758f10d4d1334296212152d4f4982f3
mathieui <mathieui@mathieui.net>
2017-10-12 00:17:09 +0200

    Improve the poopt test

--- test/test_poopt.py
+++ test/test_poopt.py
@@ -11,6 +11,8 @@ def test_cut_text():
     text = '12345678901234567890'
     assert cut_text(text, 5) == [(0, 5), (5, 10), (10, 15), (15, 20)]
 
-def test_cut_text():
     text = 'a\nb\nc\nd'
     assert cut_text(text, 10) == [(0, 2), (2, 4), (4, 6), (6, 7)]
+
+    text = 'vivent les réfrigérateurs'
+    assert cut_text(text, 6) == [(0, 6), (6, 10), (11, 17), (17, 23), (23, 25)]
