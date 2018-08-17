47bd3ab811f0e28e49a78bf5bcddbeb77c570329
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2018-08-17 18:48:39 +0100

    windows.misc: Type everything in this module.

--- poezio/windows/misc.py
+++ poezio/windows/misc.py
@@ -3,12 +3,14 @@ Wins that don’t fit any category
 """
 
 import logging
-log = logging.getLogger(__name__)
-
 import curses
+
+from typing import List
 
 from poezio.windows.base_wins import Win
 from poezio.theming import get_theme, to_curses_attr
+
+log = logging.getLogger(__name__)
 
 
 class VerticalSeparator(Win):
@@ -17,23 +19,23 @@ class VerticalSeparator(Win):
     refreshed only on resize, but never on refresh, for efficiency
     """
 
-    def rewrite_line(self):
+    def rewrite_line(self) -> None:
         self._win.vline(0, 0, curses.ACS_VLINE, self.height,
                         to_curses_attr(get_theme().COLOR_VERTICAL_SEPARATOR))
         self._refresh()
 
-    def refresh(self):
+    def refresh(self) -> None:
         log.debug('Refresh: %s', self.__class__.__name__)
         self.rewrite_line()
 
 
 class SimpleTextWin(Win):
-    def __init__(self, text):
+    def __init__(self, text) -> None:
         Win.__init__(self)
         self._text = text
-        self.built_lines = []
+        self.built_lines = []  # type: List[str]
 
-    def rebuild_text(self):
+    def rebuild_text(self) -> None:
         """
         Transform the text in lines than can then be
         displayed without any calculation or anything
@@ -50,7 +52,7 @@ class SimpleTextWin(Win):
                 line = line[limit:]
             self.built_lines.append(line)
 
-    def refresh(self):
+    def refresh(self) -> None:
         log.debug('Refresh: %s', self.__class__.__name__)
         self._win.erase()
         for y, line in enumerate(self.built_lines):
