ced7e0e81e4a161c8c38a003141362db522c1b06
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2018-10-17 15:13:39 +0200

    Do formatted text printing in Rust.

--- poezio/windows/base_wins.py
+++ poezio/windows/base_wins.py
@@ -12,12 +12,9 @@ TAB_WIN = None
 import logging
 log = logging.getLogger(__name__)
 
-import curses
-import string
-
 from typing import Optional, Tuple
 
-from poezio.theming import to_curses_attr, read_tuple
+from poezio import libpoezio
 
 FORMAT_CHAR = '\x19'
 # These are non-printable chars, so they should never appear in the input,
@@ -98,65 +95,14 @@ class Win:
         For example:
         \x19bhello → hello in bold
         \x191}Bonj\x192}our → 'Bonj' in red and 'our' in green
-        next_attr_char is the \x19 delimiter
-        attr_char is the char following it, it can be
-        one of 'u', 'b', 'i', 'c[0-9]'
         """
         if y is not None and x is not None:
             self.move(y, x)
-        next_attr_char = text.find(FORMAT_CHAR)
-        attr_italic = curses.A_ITALIC if hasattr(
-            curses, 'A_ITALIC') else curses.A_REVERSE
-        while next_attr_char != -1 and text:
-            if next_attr_char + 1 < len(text):
-                attr_char = text[next_attr_char + 1].lower()
-            else:
-                attr_char = str()
-            if next_attr_char != 0:
-                self.addstr(text[:next_attr_char])
-            if attr_char == 'o':
-                self._win.attrset(0)
-            elif attr_char == 'u':
-                self._win.attron(curses.A_UNDERLINE)
-            elif attr_char == 'b':
-                self._win.attron(curses.A_BOLD)
-            elif attr_char == 'i':
-                self._win.attron(attr_italic)
-            if (attr_char in string.digits
-                    or attr_char == '-') and attr_char != '':
-                color_str = text[next_attr_char +
-                                 1:text.find('}', next_attr_char)]
-                if ',' in color_str:
-                    tup, char = read_tuple(color_str)
-                    self._win.attron(to_curses_attr(tup))
-                    if char:
-                        if char == 'o':
-                            self._win.attrset(0)
-                        elif char == 'u':
-                            self._win.attron(curses.A_UNDERLINE)
-                        elif char == 'b':
-                            self._win.attron(curses.A_BOLD)
-                        elif char == 'i':
-                            self._win.attron(attr_italic)
-                    else:
-                        # this will reset previous bold/uderline sequences if any was used
-                        self._win.attroff(curses.A_UNDERLINE)
-                        self._win.attroff(curses.A_BOLD)
-                elif color_str:
-                    self._win.attron(to_curses_attr((int(color_str), -1)))
-                text = text[next_attr_char + len(color_str) + 2:]
-            else:
-                text = text[next_attr_char + 2:]
-            next_attr_char = text.find(FORMAT_CHAR)
-        self.addstr(text)
+        # XXX: the cancel attribute is a hack for nom’s Incomplete issue.
+        libpoezio.printw(self._win, text + '\x19o')
 
     def finish_line(self, color: Optional[Tuple] = None) -> None:
         """
         Write colored spaces until the end of line
         """
-        (y, x) = self._win.getyx()
-        size = self.width - x
-        if color:
-            self.addnstr(' ' * size, size, to_curses_attr(color))
-        else:
-            self.addnstr(' ' * size, size)
+        libpoezio.finish_line(self._win, self.width, color);
