86c0e32bb64ceb956ff793c32f70b2c1ff28f341
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2018-10-17 15:13:39 +0200

    Do formatted text printing in Rust.

--- test/test_theming.py
+++ test/test_theming.py
@@ -4,17 +4,7 @@ Test the functions in the `theming` module
 
 import pytest
 
-from poezio.theming import dump_tuple, read_tuple
-
-def test_read_tuple():
-    assert read_tuple('1,-1,u') == ((1, -1), 'u')
-    assert read_tuple('1,2') == ((1, 2), '\0')
-
-    with pytest.raises(IndexError):
-        read_tuple('1')
-
-    with pytest.raises(ValueError):
-        read_tuple('toto')
+from poezio.theming import dump_tuple
 
 def test_dump_tuple():
     assert dump_tuple((1, 2)) == '1,2'
