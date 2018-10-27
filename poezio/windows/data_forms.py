poezio/poezio

commit cce1a4090a832a6fd2b7be4c610e469efb480b15

Add __slots__ in most of the poezio.windows classes, to be more explicit about their data.

poezio/windows/data_forms.py
@@ -20,6 +20,9 @@ class FieldInput:
     'windows' library.
     """
 
+    # XXX: This conflicts with Win in the FieldInputMixin.
+    #__slots__ = ('_field', 'color')
+
     def __init__(self, field):
         self._field = field
         self.color = get_theme().COLOR_NORMAL_TEXT
@@ -47,6 +50,8 @@ class FieldInput:
 class FieldInputMixin(FieldInput, Win):
     "Mix both FieldInput and Win"
 
+    __slots__ = ()
+
     def __init__(self, field):
         FieldInput.__init__(self, field)
         Win.__init__(self)
@@ -60,6 +65,8 @@ class FieldInputMixin(FieldInput, Win):
 
 
 class ColoredLabel(Win):
+    __slots__ = ('text', 'color')
+
     def __init__(self, text):
         self.text = text
         self.color = get_theme().COLOR_NORMAL_TEXT
@@ -85,6 +92,8 @@ class DummyInput(FieldInputMixin):
     Used for fields that do not require any input ('fixed')
     """
 
+    __slots__ = ()
+
     def __init__(self, field):
         FieldInputMixin.__init__(self, field)
 
@@ -99,6 +108,8 @@ class DummyInput(FieldInputMixin):
 
 
 class BooleanWin(FieldInputMixin):
+    __slots__ = ('last_key', 'value')
+
     def __init__(self, field):
         FieldInputMixin.__init__(self, field)
         self.last_key = 'KEY_RIGHT'
@@ -133,6 +144,8 @@ class BooleanWin(FieldInputMixin):
 
 
 class TextMultiWin(FieldInputMixin):
+    __slots__ = ('options', 'val_pos', 'edition_input')
+
     def __init__(self, field):
         FieldInputMixin.__init__(self, field)
         options = field.get_value()
@@ -212,6 +225,8 @@ class TextMultiWin(FieldInputMixin):
 
 
 class ListMultiWin(FieldInputMixin):
+    __slots__ = ('options', 'val_pos')
+
     def __init__(self, field):
         FieldInputMixin.__init__(self, field)
         values = field.get_value() or []
@@ -261,6 +276,8 @@ class ListMultiWin(FieldInputMixin):
 
 
 class ListSingleWin(FieldInputMixin):
+    __slots__ = ('options', 'val_pos')
+
     def __init__(self, field):
         FieldInputMixin.__init__(self, field)
         # the option list never changes
@@ -306,6 +323,8 @@ class ListSingleWin(FieldInputMixin):
 
 
 class TextSingleWin(FieldInputMixin, Input):
+    __slots__ = ('text', 'pos')
+
     def __init__(self, field):
         FieldInputMixin.__init__(self, field)
         Input.__init__(self)
@@ -323,6 +342,8 @@ class TextSingleWin(FieldInputMixin, Input):
 
 
 class TextPrivateWin(TextSingleWin):
+    __slots__ = ()
+
     def __init__(self, field):
         TextSingleWin.__init__(self, field)
 
@@ -352,6 +373,8 @@ class FormWin:
     On resize, move and resize all the subwin and define how the text will be written
     On refresh, write all the text, and refresh all the subwins
     """
+    __slots__ = ('_form', '_win', 'scroll_pos', 'current_input', 'inputs')
+
     input_classes = {
         'boolean': BooleanWin,
         'fixed': DummyInput,

poezio/windows/base_wins.py
@@ -37,6 +37,8 @@ class DummyWin:
 
 
 class Win:
+    __slots__ = ('_win', 'height', 'width', 'y', 'x')
+
     def __init__(self) -> None:
         self._win = None
         self.height, self.width = 0, 0

poezio/windows/bookmark_forms.py
@@ -152,6 +152,9 @@ class BookmarkAutojoinWin(FieldInputMixin):
 
 
 class BookmarksWin(Win):
+    __slots__ = ('scroll_pos', '_current_input', 'current_horizontal_input',
+                 '_bookmarks', 'lines')
+
     def __init__(self, bookmarks: BookmarkList, height: int, width: int, y: int, x: int) -> None:
         self._win = base_wins.TAB_WIN.derwin(height, width, y, x)
         self.scroll_pos = 0

poezio/windows/confirm.py
@@ -4,6 +4,8 @@ from poezio.theming import get_theme, to_curses_attr
 
 
 class Dialog(Win):
+    __slots__ = ('text', 'accept', 'critical')
+
     str_accept = " Accept "
     str_refuse = " Reject "
 

poezio/windows/image.py
@@ -24,6 +24,8 @@ class ImageWin(Win):
     A window which contains either an image or a border.
     """
 
+    __slots__ = ('_image', '_display_avatar')
+
     def __init__(self) -> None:
         self._image = None  # type: Optional[Image]
         Win.__init__(self)

poezio/windows/info_bar.py
@@ -16,6 +16,8 @@ from poezio.theming import get_theme, to_curses_attr
 
 
 class GlobalInfoBar(Win):
+    __slots__ = ('core')
+
     def __init__(self, core) -> None:
         Win.__init__(self)
         self.core = core
@@ -63,6 +65,8 @@ class GlobalInfoBar(Win):
 
 
 class VerticalGlobalInfoBar(Win):
+    __slots__ = ('core')
+
     def __init__(self, core, scr) -> None:
         Win.__init__(self)
         self.core = core

poezio/windows/info_wins.py
@@ -20,6 +20,8 @@ class InfoWin(Win):
     MucInfoWin, etc. Provides some useful methods.
     """
 
+    __slots__ = ()
+
     def __init__(self):
         Win.__init__(self)
 
@@ -39,6 +41,8 @@ class XMLInfoWin(InfoWin):
     """
     Info about the latest xml filter used and the state of the buffer.
     """
+
+    __slots__ = ()
 
     def __init__(self):
         InfoWin.__init__(self)
@@ -62,6 +66,8 @@ class PrivateInfoWin(InfoWin):
     The line above the information window, displaying information
     about the MUC user we are talking to
     """
+
+    __slots__ = ()
 
     def __init__(self):
         InfoWin.__init__(self)
@@ -104,6 +110,8 @@ class MucListInfoWin(InfoWin):
     about the muc server being listed
     """
 
+    __slots__ = ('message')
+
     def __init__(self, message=''):
         InfoWin.__init__(self)
         self.message = message
@@ -128,6 +136,8 @@ class ConversationInfoWin(InfoWin):
     The line above the information window, displaying information
     about the user we are talking to
     """
+
+    __slots__ = ()
 
     def __init__(self):
         InfoWin.__init__(self)
@@ -218,6 +228,8 @@ class ConversationInfoWin(InfoWin):
 
 
 class DynamicConversationInfoWin(ConversationInfoWin):
+    __slots__ = ()
+
     def write_contact_jid(self, jid):
         """
         Just displays the resource in an other color
@@ -239,6 +251,8 @@ class MucInfoWin(InfoWin):
     The line just above the information window, displaying information
     about the MUC we are viewing
     """
+
+    __slots__ = ()
 
     def __init__(self):
         InfoWin.__init__(self)
@@ -306,6 +320,8 @@ class ConversationStatusMessageWin(InfoWin):
     The upper bar displaying the status message of the contact
     """
 
+    __slots__ = ()
+
     def __init__(self):
         InfoWin.__init__(self)
 
@@ -331,6 +347,8 @@ class ConversationStatusMessageWin(InfoWin):
 
 
 class BookmarksInfoWin(InfoWin):
+    __slots__ = ()
+
     def __init__(self):
         InfoWin.__init__(self)
 
@@ -347,6 +365,8 @@ class BookmarksInfoWin(InfoWin):
 
 
 class ConfirmStatusWin(Win):
+    __slots__ = ('text', 'critical')
+
     def __init__(self, text, critical=False):
         Win.__init__(self)
         self.text = text

poezio/windows/input_placeholders.py
@@ -19,6 +19,8 @@ class HelpText(Win):
     command mode.
     """
 
+    __slots__ = ('txt')
+
     def __init__(self, text: str = '') -> None:
         Win.__init__(self)
         self.txt = text  # type: str

commit ae12b95f92d15b9a9f13752ff9e110fc6c7212e9

Add missing height and width attributes to FormWin
Fixes a regression introduced in
cce1a4090a832a6fd2b7be4c610e469efb480b15 when displaying data forms.

poezio/windows/data_forms.py
@@ -373,7 +373,7 @@ class FormWin:
     On resize, move and resize all the subwin and define how the text will be written
     On refresh, write all the text, and refresh all the subwins
     """
-    __slots__ = ('_form', '_win', 'scroll_pos', 'current_input', 'inputs')
+    __slots__ = ('_win', 'height', 'width', '_form', 'scroll_pos', 'current_input', 'inputs')
 
     input_classes = {
         'boolean': BooleanWin,