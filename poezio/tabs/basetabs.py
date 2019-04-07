89a61b84bdb29d3df8c08436c3484fddba8d2ef1
Maxime “pep” Buquet <pep@bouah.net>
2019-04-07 22:21:29 +0100

    poezio/tabs/basetabs: remove safeJID call
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- poezio/tabs/basetabs.py
+++ poezio/tabs/basetabs.py
@@ -522,8 +522,9 @@ class ChatTab(Tab):
         raise NotImplementedError
 
     def load_logs(self, log_nb: int) -> Optional[List[Dict[str, Any]]]:
-        logs = logger.get_logs(safeJID(self.name).bare, log_nb)
-        return logs
+        if self.name is not None:
+            return logger.get_logs(self.name.bare, log_nb)
+        return None
 
     def log_message(self,
                     txt: str,
