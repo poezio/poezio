042ee1fc32f7d2e97ed7476321a48eeb32b9eb51
Maxime “pep” Buquet <pep@bouah.net>
2019-06-06 13:21:29 +0200

    WIP: E2EE plugin interface
    Features:
    - Decryption by default once the plugin is loaded if messages contains
    the right EME magic
    - Encryption of messages only if encryption is enabled for the current
    tab
    
    Missing pieces:
    - No special treatment done on the order of the treatment for messages
    yet
    - No special handling of non-Partial(/Full) Stanza Encryption yet (i.e.,
    removing stuff other than <body/>)
    
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

---
+++ plugins/b64.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Maxime “pep” Buquet <pep@bouah.net>
+#
+# Distributed under terms of the zlib license.
+
+"""
+Usage
+-----
+
+Base64 encryption plugin.
+
+This plugin also respects security guidelines listed in XEP-0419.
+
+.. glossary::
+    /b64
+        **Usage:** ``/b64``
+
+        This command enables encryption of outgoing messages for the current
+        tab.
+"""
+
+from base64 import b64decode, b64encode
+from poezio.plugin_e2ee import E2EEPlugin
+from slixmpp import Message
+
+
+class Plugin(E2EEPlugin):
+    """Base64 Plugin"""
+
+    encryption_name = 'base64'
+    encryption_short_name = 'b64'
+    eme_ns = 'urn:xmpps:base64:0'
+
+    def decrypt(self, message: Message, _tab) -> None:
+        """
+            Decrypt base64
+        """
+        body = message['body']
+        message['body'] = b64decode(body.encode()).decode()
+
+    def encrypt(self, message: Message, _tab) -> None:
+        """
+            Encrypt to base64
+        """
+        # TODO: Stop using <body/> for this. Put the encoded payload in another element.
+        body = message['body']
+        message['body'] = b64encode(body.encode()).decode()
