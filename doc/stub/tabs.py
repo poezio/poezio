0c60f1f79a1d2e6ea7e60cf835a26f1b7240ff35
Madhur Garg <madhurgarg96@gmail.com>
2019-06-26 06:06:43 +0530

    Added documentation for MAM tab.

--- doc/stub/tabs.py
+++ doc/stub/tabs.py
@@ -5,6 +5,7 @@ class MucTab(ChatTab): pass
 class ConversationTab(ChatTab): pass
 class RosterInfoTab(Tab): pass
 class XMLTab(Tab): pass
+class MAMTab(Tab): pass
 class DynamicConversationTab(Tab): pass
 class StaticConversationTab(Tab): pass
 class GapTab(Tab): pass
