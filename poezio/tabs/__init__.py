ab83917d5f30a2b3fa92e702a4d120f266f05a14
Madhur Garg <madhurgarg96@gmail.com>
2019-06-26 06:04:46 +0530

    Added MAM info window to display JID of the current MAM query.

--- poezio/tabs/__init__.py
+++ poezio/tabs/__init__.py
@@ -7,6 +7,7 @@ from poezio.tabs.confirmtab import ConfirmTab
 from poezio.tabs.conversationtab import ConversationTab, StaticConversationTab,\
         DynamicConversationTab
 from poezio.tabs.xmltab import XMLTab
+from poezio.tabs.mamtab import MAMTab
 from poezio.tabs.listtab import ListTab
 from poezio.tabs.muclisttab import MucListTab
 from poezio.tabs.adhoc_commands_list import AdhocCommandsListTab
@@ -17,6 +18,6 @@ __all__ = [
     'Tab', 'ChatTab', 'GapTab', 'OneToOneTab', 'STATE_PRIORITY', 'SHOW_NAME',
     'RosterInfoTab', 'MucTab', 'NS_MUC_USER', 'PrivateTab', 'ConfirmTab',
     'ConversationTab', 'StaticConversationTab', 'DynamicConversationTab',
-    'XMLTab', 'ListTab', 'MucListTab', 'AdhocCommandsListTab', 'DataFormsTab',
+    'XMLTab', 'MAMTab', 'ListTab', 'MucListTab', 'AdhocCommandsListTab', 'DataFormsTab',
     'BookmarksTab'
 ]
