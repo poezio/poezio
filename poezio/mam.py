ed1fa9369b773713f6fcca5b619e731b597179f0
Madhur Garg <madhurgarg96@gmail.com>
2019-07-01 14:04:03 +0530

    Removed MAM class and changed it into function.

--- poezio/mam.py
+++ poezio/mam.py
@@ -6,11 +6,11 @@
     XEP-0313: Message Archive Management(MAM).
 """
 
-import asyncio
 from datetime import datetime, timezone
 from poezio.theming import get_theme
 from poezio.text_buffer import Message, TextBuffer
 
+<<<<<<< HEAD
 
 def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str):
     """Adds a textual entry in the TextBuffer"""
@@ -60,3 +60,36 @@ class MAM:
 
         self.tab.text_win.pos = 0
         self.tab.core.refresh_window()
+=======
+async def MAM(self, remote_jid, start, end):
+    self.remote_jid = remote_jid
+    self.start_date = start
+    self.end_date = end
+    text_buffer = self._text_buffer
+    results = self.core.xmpp.plugin['xep_0313'].retrieve(jid=self.remote_jid,
+    iterator=True, start=self.start_date, end=self.end_date)
+    async for rsm in results:
+        for msg in rsm['mam']['results']:
+            forwarded = msg['mam_result']['forwarded']
+            timestamp = forwarded['delay']['stamp']
+            message = forwarded['stanza']
+            text = str(message['body'])
+            time = datetime.strftime(timestamp, '%Y-%m-%d %H:%M:%S')
+            time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+            time = time.replace(tzinfo=timezone.utc).astimezone(tz=None)
+            nick = str(message['from']).split('/')[1]
+            color = get_theme().COLOR_OWN_NICK
+            text_buffer.add_message(
+                text,
+                time,
+                nick,
+                color,
+                True,  # History
+                None,  # User
+                False,  # Highlight
+                None,  # Identifier
+                None,  # str_time
+                None,  # Jid
+            )
+            self.core.refresh_window()
+>>>>>>> 208c814d... Removed MAM class and changed it into function.
