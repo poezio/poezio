678206fe300999a96464aee048fc7d7703a9248f
Madhur Garg <madhurgarg96@gmail.com>
2019-07-05 13:11:41 +0530

    Added fuction for infinite scroll, limited number of messages per request to 10.

--- poezio/mam.py
+++ poezio/mam.py
@@ -6,20 +6,31 @@
     XEP-0313: Message Archive Management(MAM).
 """
 
-from datetime import datetime, timezone
+import asyncio
+from datetime import datetime, timedelta, timezone
 from poezio.theming import get_theme
 from poezio.text_buffer import Message, TextBuffer
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str):
+=======
+def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str, top: bool):
+>>>>>>> dcdcc963... Added fuction for infinite scroll, limited number of messages per request to 10.
     """Adds a textual entry in the TextBuffer"""
 
     time = datetime.strftime(str_time, '%Y-%m-%d %H:%M:%S')
     time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+<<<<<<< HEAD
     time = time.replace(tzinfo=timezone.utc).astimezone(tz=None)
     nick = nick.split('/')[1]
     color = get_theme().COLOR_OWN_NICK
+=======
+    nick = nick.split('/')[1]
+    color = get_theme().COLOR_OWN_NICK
+    top = top
+>>>>>>> dcdcc963... Added fuction for infinite scroll, limited number of messages per request to 10.
     text_buffer.add_message(
         text,
         time,
@@ -28,11 +39,16 @@ def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str):
         True,  # History
         None,  # User
         False,  # Highlight
+<<<<<<< HEAD
+=======
+        top, #Top
+>>>>>>> dcdcc963... Added fuction for infinite scroll, limited number of messages per request to 10.
         None,  # Identifier
         None,  # str_time
         None,  # Jid
     )
 
+<<<<<<< HEAD
 
 class MAM:
     """
@@ -62,13 +78,19 @@ class MAM:
         self.tab.core.refresh_window()
 =======
 async def MAM(self, remote_jid, start, end):
+=======
+async def MAM(self, remote_jid, start, end, top):
+>>>>>>> dcdcc963... Added fuction for infinite scroll, limited number of messages per request to 10.
     self.remote_jid = remote_jid
     self.start_date = start
     self.end_date = end
     text_buffer = self._text_buffer
     results = self.core.xmpp.plugin['xep_0313'].retrieve(jid=self.remote_jid,
-    iterator=True, start=self.start_date, end=self.end_date)
+    iterator=True, reverse=top, start=self.start_date, end=self.end_date)
+    msg_count = 0
+    msgs = []
     async for rsm in results:
+<<<<<<< HEAD
         for msg in rsm['mam']['results']:
             forwarded = msg['mam_result']['forwarded']
             timestamp = forwarded['delay']['stamp']
@@ -93,3 +115,43 @@ async def MAM(self, remote_jid, start, end):
             )
             self.core.refresh_window()
 >>>>>>> 208c814d... Removed MAM class and changed it into function.
+=======
+        if top:
+            for msg in rsm['mam']['results']:
+                msgs.append(msg)
+                if msg_count == 10:
+                    return
+                msg_count += 1
+            msgs.reverse()
+            for msg in msgs:
+                forwarded = msg['mam_result']['forwarded']
+                timestamp = forwarded['delay']['stamp']
+                message = forwarded['stanza']
+                add_line(text_buffer, '%s' % message['body'], timestamp, str(message['from']), top)
+                self.text_win.scroll_up(len(self.text_win.built_lines))
+                self.core.refresh_window()
+        else:
+            for msg in rsm['mam']['results']:
+                forwarded = msg['mam_result']['forwarded']
+                timestamp = forwarded['delay']['stamp']
+                message = forwarded['stanza']
+                add_line(text_buffer, '%s' % message['body'], timestamp, str(message['from']), top)
+                self.core.refresh_window()
+
+
+def mam_scroll(self):
+    remote_jid = self.jid
+    text_buffer = self._text_buffer
+    end = datetime.now()
+    for message in text_buffer.messages:
+        time = message.time
+        if time < end:
+            end = time
+    end = end + timedelta(seconds=-1)
+    end = datetime.strftime(end, '%Y-%m-%dT%H:%M:%SZ')
+    start = datetime.strptime(end, '%Y-%m-%dT%H:%M:%SZ')
+    start = start + timedelta(days=-10)
+    start = datetime.strftime(start, '%Y-%m-%dT%H:%M:%SZ')
+    top = True
+    asyncio.ensure_future(MAM(self, remote_jid, start, end, top))
+>>>>>>> dcdcc963... Added fuction for infinite scroll, limited number of messages per request to 10.
