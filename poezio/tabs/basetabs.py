4f9038c8bc1bb41bbd99abcdd48c179786833541
Madhur Garg <madhurgarg96@gmail.com>
2019-07-16 00:54:40 +0530

    Fixed repeated query for messages on pressing PageUp

--- poezio/tabs/basetabs.py
+++ poezio/tabs/basetabs.py
@@ -474,6 +474,7 @@ class ChatTab(Tab):
             jid = JID(jid)
         assert jid.domain
         self._jid = jid
+        self.query_id = 0
 
         self._name = jid.full  # type: Optional[str]
         self.text_win = None
@@ -959,7 +960,10 @@ class ChatTab(Tab):
         return self.text_win.scroll_down(1)
 
     def on_scroll_up(self):
-        return mam.mam_scroll(self)
+        if self.query_id == 0:
+            return mam.mam_scroll(self)
+        else:
+            return self.text_win.scroll_up(self.text_win.height - 1)
 
     def on_scroll_down(self):
         return self.text_win.scroll_down(self.text_win.height - 1)
