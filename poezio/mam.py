2f1af846d6bcd6cb78ed0a7fe0b93414e8602c33
Madhur Garg <madhurgarg96@gmail.com>
2019-07-24 02:22:27 +0530

    Added a check for tabs (because there is a different way to query messages for MUC and any other type of tab)

--- poezio/mam.py
+++ poezio/mam.py
@@ -9,8 +9,10 @@
 import asyncio
 from datetime import datetime, timedelta, timezone
 from poezio.theming import get_theme
+from poezio import tabs
 from poezio.text_buffer import Message, TextBuffer
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 
@@ -18,6 +20,9 @@ def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str):
 =======
 def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str, top: bool):
 >>>>>>> dcdcc963... Added fuction for infinite scroll, limited number of messages per request to 10.
+=======
+def add_line(self, text_buffer: TextBuffer, text: str, str_time: str, nick: str, top: bool):
+>>>>>>> ef48615c... Added a check for tabs (because there is a different way to query messages for MUC and any other type of tab)
     """Adds a textual entry in the TextBuffer"""
 
     time = datetime.strftime(str_time, '%Y-%m-%d %H:%M:%S')
@@ -32,7 +37,10 @@ def add_line(text_buffer: TextBuffer, text: str, str_time: str, nick: str, top: 
     color = get_theme().COLOR_OWN_NICK
 =======
     if '/' in nick:
-        nick = nick.split('/')[1]
+        if isinstance(self, tabs.MucTab):
+            nick = nick.split('/')[1]
+        else:
+            nick = nick.split('/')[0]
         color = get_theme().COLOR_OWN_NICK
         time = time.replace(tzinfo=timezone.utc).astimezone(tz=None)
         time = time.replace(tzinfo=None)
@@ -106,15 +114,23 @@ async def query(self, remote_jid, start, end, top):
     if 'urn:xmpp:mam:2' not in iq['disco_info'].get_features():
         return self.core.information("This MUC doesn't support MAM.", "Error")
     if top:
-        results = self.core.xmpp['xep_0313'].retrieve(jid=self.remote_jid,
-        iterator=True, reverse=top, end=self.end_date)
+        if isinstance(self, tabs.MucTab):
+            results = self.core.xmpp['xep_0313'].retrieve(jid=self.remote_jid,
+            iterator=True, reverse=top, end=self.end_date)
+        else:
+            results = self.core.xmpp['xep_0313'].retrieve(with_jid=self.remote_jid,
+            iterator=True, reverse=top, end=self.end_date)
     else:
-        results = self.core.xmpp['xep_0313'].retrieve(jid=self.remote_jid,
-        iterator=True, reverse=top, start=self.start_date, end=self.end_date)
+        if isinstance(self, tabs.MucTab):
+            results = self.core.xmpp['xep_0313'].retrieve(jid=self.remote_jid,
+            iterator=True, reverse=top, start=self.start_date, end=self.end_date)
+        else:
+            results = self.core.xmpp['xep_0313'].retrieve(with_jid=self.remote_jid,
+            iterator=True, reverse=top, start=self.start_date, end=self.end_date)
     msg_count = 0
     msgs = []
     timestamp = datetime.now()
-    add_line(text_buffer, 'Start of MAM query: ', timestamp, 'MAM', top)
+    add_line(self, text_buffer, 'Start of MAM query: ', timestamp, 'MAM', top)
     async for rsm in results:
 <<<<<<< HEAD
         for msg in rsm['mam']['results']:
@@ -148,7 +164,7 @@ async def query(self, remote_jid, start, end, top):
                 if msg_count == 10:
                     self.query_id = 0
                     timestamp = datetime.now()
-                    add_line(text_buffer, 'End of MAM query: ', timestamp, 'MAM', top)
+                    add_line(self, text_buffer, 'End of MAM query: ', timestamp, 'MAM', top)
                     self.core.refresh_window()
                     return
                 msg_count += 1
@@ -158,22 +174,26 @@ async def query(self, remote_jid, start, end, top):
                 timestamp = forwarded['delay']['stamp']
                 message = forwarded['stanza']
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
                 add_line(text_buffer, '%s' % message['body'], timestamp, str(message['from']))
                 self.tab.core.refresh_window()
 >>>>>>> 3de40ea3... Merged changes from mam branchg
                 add_line(text_buffer, message['body'], timestamp, str(message['from']), top)
+=======
+                add_line(self, text_buffer, message['body'], timestamp, str(message['from']), top)
+>>>>>>> ef48615c... Added a check for tabs (because there is a different way to query messages for MUC and any other type of tab)
                 self.text_win.scroll_up(len(self.text_win.built_lines))
         else:
             for msg in rsm['mam']['results']:
                 forwarded = msg['mam_result']['forwarded']
                 timestamp = forwarded['delay']['stamp']
                 message = forwarded['stanza']
-                add_line(text_buffer, message['body'], timestamp, str(message['from']), top)
+                add_line(self, text_buffer, message['body'], timestamp, str(message['from']), top)
                 self.core.refresh_window()
     self.query_id = 0
     timestamp = datetime.now()
-    add_line(text_buffer, 'End of MAM query: ', timestamp, 'MAM', top)
+    add_line(self, text_buffer, 'End of MAM query: ', timestamp, 'MAM', top)
 
 
 def mam_scroll(self):
