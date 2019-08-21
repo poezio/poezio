9874c3bbf71234c46bd83ff69a12ae7698daa6d6
Madhur Garg <madhurgarg96@gmail.com>
2019-08-21 13:21:02 +0530

    Changed stanza_id attribute to last_stanza_id.

--- poezio/tabs/basetabs.py
+++ poezio/tabs/basetabs.py
@@ -475,7 +475,7 @@ class ChatTab(Tab):
         assert jid.domain
         self._jid = jid
         self.query_id = 0
-        self.stanza_id = None
+        self.last_stanza_id = None
 
         self._name = jid.full  # type: Optional[str]
         self.text_win = None
