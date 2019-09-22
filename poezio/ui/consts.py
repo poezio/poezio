c99041a6ee6a8fe3824616b065cde5a7dafc153f
mathieui <mathieui@mathieui.net>
2019-09-22 17:35:07 +0200

    Move message rendering code to Message.render()
    Also:
    
    - rename format_chars to FORMAT_CHARS because it’s static constant
    - move Line, Message, and a few funcs/consts to a new poezio.ui module

---
+++ poezio/ui/consts.py
@@ -0,0 +1,4 @@
+FORMAT_CHAR = '\x19'
+# These are non-printable chars, so they should never appear in the input,
+# I guess. But maybe we can find better chars that are even less risky.
+FORMAT_CHARS = '\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x1A'
