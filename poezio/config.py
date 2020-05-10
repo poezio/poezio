d741bda6331a4d04190a093f111a2231056adcca
Jonas Schäfer <j.wielicki@sotecware.net>
2020-05-10 11:21:56 +0200

    Add option to match tabs only by prefix in /win

--- poezio/config.py
+++ poezio/config.py
@@ -137,6 +137,7 @@ DEFAULT_CONFIG = {
         'themes_dir': '',
         'tmp_image_dir': '',
         'unique_prefix_tab_names': False,
+        'unique_prefix_tab_names_match': False,
         'use_bookmarks_method': '',
         'use_log': True,
         'use_remote_bookmarks': True,
