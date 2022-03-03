cd8ae9f4727c2019cd55b4711d9c02cfad1a2afd
Maxime “pep” Buquet <pep@bouah.net>
2022-03-03 23:51:56 +0100

    rustfmt
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- src/lib.rs
+++ src/lib.rs
@@ -9,24 +9,38 @@ extern crate enum_set;
 
 pub mod theming;
 
-use cpython::{Python, PyResult, PyErr, PyObject, ToPyObject, PythonObject};
 use self::theming::{curses_attr, parse_attrs};
+use cpython::{PyErr, PyObject, PyResult, Python, PythonObject, ToPyObject};
 
 py_module_initializer!(libpoezio, initlibpoezio, PyInit_libpoezio, |py, m| {
-    m.add(py, "to_curses_attr", py_fn!(py, to_curses_attr(fg: i16, bg: i16, attrs: &str)))?;
+    m.add(
+        py,
+        "to_curses_attr",
+        py_fn!(py, to_curses_attr(fg: i16, bg: i16, attrs: &str)),
+    )?;
     Ok(())
 });
 
 py_exception!(libpoezio, LogParseError);
 
 macro_rules! py_int {
-    ($py:ident, $i:expr) => ($i.to_py_object($py).into_object())
+    ($py:ident, $i:expr) => {
+        $i.to_py_object($py).into_object()
+    };
 }
 
 fn nom_to_py_err(py: Python, err: nom::Err<&str>) -> PyErr {
     PyErr {
         ptype: py.get_type::<LogParseError>().into_object(),
-        pvalue: Some(LogParseError(err.into_error_kind().description().to_py_object(py).into_object()).into_object()),
+        pvalue: Some(
+            LogParseError(
+                err.into_error_kind()
+                    .description()
+                    .to_py_object(py)
+                    .into_object(),
+            )
+            .into_object(),
+        ),
         ptraceback: None,
     }
 }
