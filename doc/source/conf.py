47caee0bed1201f1ede7761609f00ca6ea3a98ab
Maxime “pep” Buquet <pep@bouah.net>
2022-04-10 15:05:24 +0200

    Release 0.14
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,9 +53,9 @@ copyright = '%s, Mathieu Pasquet - Florent Le Coz - Emmanuel Gil Peyrot - Maxime
 # built documents.
 #
 # The short X.Y version.
-version = '0.13'
+version = '0.14'
 # The full version, including alpha/beta/rc tags.
-release = '0.13.1'
+release = '0.14'
 
 add_function_parentheses = True
 
